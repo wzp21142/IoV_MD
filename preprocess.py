df23998a2454f17f32bdde20c5bdb2fa6ae7cb4d
LucasChen <lucaschen_lc@outlook.com>
2021-03-11 20:00:19 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -64,50 +64,9 @@ def data_convert(data):  # 数据正则与空值填充
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Demo of argparse")
-
-    parser.add_argument('-s', type=int, default=1, help="option of sorting the dataset by rcvTime")
-    args = parser.parse_args()
-    print(args)
-
-    ratio_train = 0.8  # 训练集比例
-    ratio_val = 0.1  # 验证集比例
-    ratio_test = 1 - ratio_train - ratio_val  # 测试集比例
     file_path = 'VeReMi-Dataset/000_Select_data'  # 数据集保存路径
-    dataset = read_raw_file(file_path)
-
-    if args.s:
-        data_set = dataset.sort_values(by='rcvTime', ascending=True)
-        train_set = dataset[0: int(len(data_set)*ratio_train)].copy()
-        val_set = dataset[int(len(data_set)*ratio_train): int(len(data_set)*(ratio_train+ratio_val))].copy()
-        test_set = dataset[int(len(data_set)*(ratio_train+ratio_val)): len(data_set)].copy()
-    else:
-        train_set = dataset.sample(frac=ratio_train, replace=False)
-        val_set = dataset.sample(frac=ratio_val, replace=False)
-        test_set = dataset.sample(frac=ratio_test, replace=False)
-
-    train_set.pop('rcvTime')
-    val_set.pop('rcvTime')
-    test_set.pop('rcvTime')
-    print("train sample ratio: " + str(ratio_train))
-    print("val sample ratio: " + str(ratio_val))
-    print("test sample ratio: " + str(ratio_test) + '\n')
-
-    train_set = data_convert(train_set)
-    val_set = data_convert(val_set)
-    test_set = data_convert(test_set)
-
-    if args.s:
-        prefix = "timesorted_data/"
-        suffix = "_time_sorted"
-    else:
-        prefix = "process_data/"
-        suffix = ""
-    if not os.path.exists(prefix):
-        os.mkdir(prefix)
-    train_set.to_csv(prefix + 'train_set' + suffix + '.csv')
-    val_set.to_csv(prefix + 'val_set' + suffix + '.csv')
-    test_set.to_csv(prefix + 'test_set' + suffix + '.csv')
-    print("train label ratio: " + str(len(train_set[train_set['label'] == 1])/len(train_set)))
-    print("val label ratio: " + str(len(val_set[val_set['label'] == 1])/len(val_set)))
-    print("test label ratio: " + str(len(test_set[test_set['label'] == 1])/len(test_set)))
+    df = read_raw_file(file_path)
+    df = df.drop(df.columns[df.max() == 0], axis=1)
+    df = df.sort_values(by='rcvTime', ascending=True)
+    df = data_convert(df)
+    df.to_csv('dataset.csv')
--- train.py
+++ train.py
@@ -1,24 +1,60 @@
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
+import seaborn as sns
 import tensorflow as tf
-from autoencoder import *
-from preprocess import data_convert
 
-'''single_step_window = WindowGenerator(
-    input_width=1, label_width=1, shift=1,
-    label_columns=['label'])
-single_step_window'''
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
 
 dataset_path = 'VeReMi-Dataset'
-type_path = 'timesorted_data'
+
+df = None
 for directory in os.listdir(dataset_path):
-    train_df = pd.read_csv(os.path.join(dataset_path, directory, type_path, 'train_set_time_sorted.csv'))
-    val_df = pd.read_csv(os.path.join(dataset_path, directory, type_path, 'val_set_time_sorted.csv'))
-    test_df = pd.read_csv(os.path.join(dataset_path, directory, type_path, 'test_set_time_sorted.csv'))
+    df = pd.read_csv(os.path.join(dataset_path, directory, 'dataset.csv'))
+
+df.pop('omnet_id')
+df.pop('Unnamed: 0')
+print(df.describe().transpose())
+
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+n = len(df)
+train_df = df[0:int(n * 0.7)]
+val_df = df[int(n * 0.7):int(n * 0.9)]
+test_df = df[int(n * 0.9):]
+
+train_label = train_df.pop('label')
+val_label = val_df.pop('label')
+test_label = test_df.pop('label')
+num_features = df.shape[1]
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+train_df['label'] = train_label
+val_df['label'] = val_label
+test_df['label'] = test_label
+
+df_std = (df - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+plt.figure(figsize=(12, 6))
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+_ = ax.set_xticklabels(df.keys(), rotation=90)
+plt.show()
 
 
-class WindowGenerator:
+class WindowGenerator():
     def __init__(self, input_width, label_width, shift,
                  train_df=train_df, val_df=val_df, test_df=test_df,
                  label_columns=None):
@@ -30,8 +66,10 @@ class WindowGenerator:
         # Work out the label column indices.
         self.label_columns = label_columns
         if label_columns is not None:
-            self.label_columns_indices = {name: i for i, name in enumerate(label_columns)}
-        self.column_indices = {name: i for i, name in enumerate(train_df.columns)}
+            self.label_columns_indices = {name: i for i, name in
+                                          enumerate(label_columns)}
+        self.column_indices = {name: i for i, name in
+                               enumerate(train_df.columns)}
 
         # Work out the window parameters.
         self.input_width = input_width
@@ -47,6 +85,90 @@ class WindowGenerator:
         self.labels_slice = slice(self.label_start, None)
         self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
 
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+                axis=-1)
+
+        # Slicing doesn't preserve static shape information, so set the shapes
+        # manually. This way the `tf.data.Datasets` are easier to inspect.
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+
+        return inputs, labels
+
+    def plot(self, model=None, plot_col='label', max_subplots=3):
+        inputs, labels = self.example
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+        for n in range(max_n):
+            plt.subplot(3, 1, n + 1)
+            plt.ylabel(f'{plot_col} [normed]')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                     label='Inputs', marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel('Time [h]')
+
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            batch_size=32, )
+
+        ds = ds.map(self.split_window)
+
+        return ds
+
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+
+    @property
+    def val(self):
+        return self.make_dataset(self.val_df)
+
+    @property
+    def test(self):
+        return self.make_dataset(self.test_df)
+
+    @property
+    def example(self):
+        """Get and cache an example batch of `inputs, labels` for plotting."""
+        result = getattr(self, '_example', None)
+        if result is None:
+            # No example batch was found, so get one from the `.train` dataset
+            result = next(iter(self.train))
+            # And cache it for next time
+            self._example = result
+        return result
+
     def __repr__(self):
         return '\n'.join([
             f'Total window size: {self.total_window_size}',
@@ -55,55 +177,10 @@ class WindowGenerator:
             f'Label column name(s): {self.label_columns}'])
 
 
-def make_dataset(self, data):
-    data = np.array(data, dtype=np.float32)
-    ds = tf.keras.preprocessing.timeseries_dataset_from_array(
-        data=data,
-        targets=None,
-        sequence_length=self.total_window_size,
-        sequence_stride=1,
-        shuffle=True,
-        batch_size=32, )
-
-    ds = ds.map(self.split_window)
-
-    return ds
-
-
-WindowGenerator.make_dataset = make_dataset
-
-
-@property
-def train(self):
-    return self.make_dataset(self.train_df)
-
-
-@property
-def val(self):
-    return self.make_dataset(self.val_df)
-
-
-@property
-def test(self):
-    return self.make_dataset(self.test_df)
-
-
-@property
-def example(self):
-    """Get and cache an example batch of `inputs, labels` for plotting."""
-    result = getattr(self, '_example', None)
-    if result is None:
-        # No example batch was found, so get one from the `.train` dataset
-        result = next(iter(self.train))
-        # And cache it for next time
-        self._example = result
-    return result
-
-
-WindowGenerator.train = train
-WindowGenerator.val = val
-WindowGenerator.test = test
-WindowGenerator.example = example
+single_step_window = WindowGenerator(
+    input_width=1, label_width=1, shift=1,
+    label_columns=['label'])
+print(single_step_window)
 
 
 class Baseline(tf.keras.Model):
@@ -118,54 +195,24 @@ class Baseline(tf.keras.Model):
         return result[:, :, tf.newaxis]
 
 
-def split_window(self, features):
-    inputs = features[:, self.input_slice, :]
-    labels = features[:, self.labels_slice, :]
-    if self.label_columns is not None:
-        labels = tf.stack(
-            [labels[:, :, self.column_indices[name]] for name in self.label_columns],
-            axis=-1)
+baseline = Baseline(label_index=column_indices['label'])
 
-    # Slicing doesn't preserve static shape information, so set the shapes
-    # manually. This way the `tf.data.Datasets` are easier to inspect.
-    inputs.set_shape([None, self.input_width, None])
-    labels.set_shape([None, self.label_width, None])
-
-    return inputs, labels
-
-
-WindowGenerator.split_window = split_window
-
-x_train, x_test, y_train, y_test = data_load()
-#AE_train(3, x_train, 10)
-x = AE_predict(x_test, y_test)
-df = pd.DataFrame(x)
-df = data_convert(df)
-df.columns = ['A', 'B', 'C', 'label']
-
-train_df = df[0: 20000]
-val_df = df[20000: 30000]
-test_df = df[30000: 40000]
-
-single_step_window = WindowGenerator(
-    input_width=1, label_width=1, shift=1,
-    label_columns=['label'])
-
-column_indices = {name: i for i, name in enumerate(df.columns)}
-'''baseline = Baseline(label_index=column_indices['label'])
-
-baseline.compile(loss='binary_crossentropy',
-                 metrics=[tf.metrics.MeanAbsoluteError()])'''
+baseline.compile(loss=tf.losses.MeanSquaredError(),
+                 metrics=[tf.metrics.MeanAbsoluteError()])
 
 val_performance = {}
 performance = {}
-# val_performance['Baseline'] = baseline.evaluate(single_step_window.val)
-# performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+val_performance['Baseline'] = baseline.evaluate(single_step_window.val)
+performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+
+wide_window = WindowGenerator(
+    input_width=24, label_width=24, shift=1,
+    label_columns=['label'])
 
 linear = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=4, activation='relu'),
-    tf.keras.layers.Dense(units=1, activation='sigmoid'),
+    tf.keras.layers.Dense(units=1)
 ])
+
 MAX_EPOCHS = 20
 
 
@@ -174,9 +221,9 @@ def compile_and_fit(model, window, patience=2):
                                                       patience=patience,
                                                       mode='min')
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer=tf.keras.optimizers.RMSprop(lr=0.005),
-                  metrics=[tf.metrics.MeanAbsoluteError(), 'accuracy'])
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanAbsoluteError()])
 
     history = model.fit(window.train, epochs=MAX_EPOCHS,
                         validation_data=window.val,
@@ -186,5 +233,90 @@ def compile_and_fit(model, window, patience=2):
 
 history = compile_and_fit(linear, single_step_window)
 
-# val_performance['Linear'] = linear.evaluate(single_step_window.val)
-# performance['Linear'] = linear.evaluate(single_step_window.test, verbose=0)
+val_performance['Linear'] = linear.evaluate(single_step_window.val)
+performance['Linear'] = linear.evaluate(single_step_window.test, verbose=0)
+
+dense = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=64, activation='relu'),
+    tf.keras.layers.Dense(units=64, activation='relu'),
+    tf.keras.layers.Dense(units=1)
+])
+
+history = compile_and_fit(dense, single_step_window)
+
+val_performance['Dense'] = dense.evaluate(single_step_window.val)
+performance['Dense'] = dense.evaluate(single_step_window.test, verbose=0)
+
+CONV_WIDTH = 3
+conv_window = WindowGenerator(
+    input_width=CONV_WIDTH,
+    label_width=1,
+    shift=1,
+    label_columns=['label'])
+
+multi_step_dense = tf.keras.Sequential([
+    # Shape: (time, features) => (time*features)
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+    # Add back the time dimension.
+    # Shape: (outputs) => (1, outputs)
+    tf.keras.layers.Reshape([1, -1]),
+])
+
+history = compile_and_fit(multi_step_dense, conv_window)
+
+IPython.display.clear_output()
+val_performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.val)
+performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.test, verbose=0)
+
+conv_model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=32,
+                           kernel_size=(CONV_WIDTH,),
+                           activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+])
+
+history = compile_and_fit(conv_model, conv_window)
+
+IPython.display.clear_output()
+val_performance['Conv'] = conv_model.evaluate(conv_window.val)
+performance['Conv'] = conv_model.evaluate(conv_window.test, verbose=0)
+
+LABEL_WIDTH = 24
+INPUT_WIDTH = LABEL_WIDTH + (CONV_WIDTH - 1)
+wide_conv_window = WindowGenerator(
+    input_width=INPUT_WIDTH,
+    label_width=LABEL_WIDTH,
+    shift=1,
+    label_columns=['label'])
+
+lstm_model = tf.keras.models.Sequential([
+    # Shape [batch, time, features] => [batch, time, lstm_units]
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    # Shape => [batch, time, features]
+    tf.keras.layers.Dense(units=1)
+])
+
+history = compile_and_fit(lstm_model, wide_window)
+
+IPython.display.clear_output()
+val_performance['LSTM'] = lstm_model.evaluate(wide_window.val)
+performance['LSTM'] = lstm_model.evaluate(wide_window.test, verbose=0)
+
+x = np.arange(len(performance))
+width = 0.3
+metric_name = 'mean_absolute_error'
+metric_index = lstm_model.metrics_names.index('mean_absolute_error')
+val_mae = [v[metric_index] for v in val_performance.values()]
+test_mae = [v[metric_index] for v in performance.values()]
+
+plt.ylabel('mean_absolute_error [T (degC), normalized]')
+plt.bar(x - 0.17, val_mae, width, label='Validation')
+plt.bar(x + 0.17, test_mae, width, label='Test')
+plt.xticks(ticks=x, labels=performance.keys(),
+           rotation=45)
+_ = plt.legend()
+plt.show()
